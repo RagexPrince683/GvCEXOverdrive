629ed6657ed3709d8a1195819bd97192153c5fdb
RagexPrince683 <yellowtoad91@gmail.com>
2025-12-30 09:15:11 -0500

    try fix 2

--- HMG/src/main/java/handmadeguns/gui/HMGGuiInventoryItem.java
+++ HMG/src/main/java/handmadeguns/gui/HMGGuiInventoryItem.java
@@ -37,35 +37,30 @@ public class HMGGuiInventoryItem extends GuiContainer
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
     {
-        // Text
+        // text (unchanged)
         this.fontRendererObj.drawString("Attachments", 8, 6, 4210752);
         this.fontRendererObj.drawString("Inventory", 8, this.ySize - 96 + 2, 4210752);
         this.fontRendererObj.drawString("Sight/Support/Muzzle/Under/SP bullets", 8, 24, 4210752);
 
-        // === STABLE CENTER ANCHOR ===
         int centerX = this.guiLeft + this.xSize / 2;
-        int centerY = this.guiTop + this.ySize / 2;
-
-        float posX = centerX;
-        float posY = centerY + 30; // intentional vertical offset
-        float scale = 60.0F;
+        int centerY = this.guiTop + this.ySize / 2 + 30; // small vertical offset if desired
+        int scale = 60; // same idea as before
 
         GL11.glEnable(GL11.GL_COLOR_MATERIAL);
         GL11.glPushMatrix();
 
-        GL11.glTranslatef(posX, posY, 120.0F);
-        GL11.glScalef(-scale, scale, scale);
+        // translate to the point we want and scale *after* translating like vanilla does
+        GL11.glTranslatef((float)centerX, (float)centerY, 50.0F);
+        GL11.glScalef((float)(-scale), (float)scale, (float)scale);
 
-        // rotations (unchanged behavior)
+        // orientation same as vanilla drawEntityOnScreen
         GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-        GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
         GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
 
         RenderHelper.enableStandardItemLighting();
-
         GL11.glRotatef(-135.0F, 0.0F, 1.0F, 0.0F);
 
-        // save + override view angle to prevent jitter
+        // temporarily force view to stable angle and keep a copy to restore
         float prevViewY = RenderManager.instance.playerViewY;
         RenderManager.instance.playerViewY = 180.0F;
 
@@ -75,10 +70,9 @@ public class HMGGuiInventoryItem extends GuiContainer
         GL11.glEnable(GL11.GL_CULL_FACE);
 
         ItemStack currentItem = ((HMGContainerInventoryItem)inventorySlots).inventory.currentItem;
-
         if (currentItem != null && currentItem.getTagCompound() != null)
         {
-            // rebuild attachment NBT for renderer
+            // rebuild attachment NBT
             NBTTagList tagList = new NBTTagList();
             for (int i = 0; i < ((HMGContainerInventoryItem)inventorySlots).inventory.items.length; i++)
             {
@@ -101,20 +95,19 @@ public class HMGGuiInventoryItem extends GuiContainer
             if (gunrender instanceof HMGRenderItemGun_U_NEW ||
                     gunrender instanceof HMGRenderItemGun_U)
             {
+                // render at origin (we already translated/scaled to where we want)
                 gunrender.renderItem(IItemRenderer.ItemRenderType.ENTITY, currentItem);
             }
 
             GL11.glDisable(GL12.GL_RESCALE_NORMAL);
         }
 
-        // restore render state
+        // restore
         RenderManager.instance.playerViewY = prevViewY;
-
         GL11.glDepthMask(false);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
 
         GL11.glPopMatrix();
-
         RenderHelper.disableStandardItemLighting();
         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
 
