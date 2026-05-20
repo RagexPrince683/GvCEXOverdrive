bcee6967b45f62a1bdd53b8bb449fdb8ef8dfd91
Zealot <39095332+GlowingFederal@users.noreply.github.com>
2026-05-20 01:51:16 -0400

    Adjust kill feed overlay layering and position

--- HMG/src/main/java/handmadeguns/HMGPacketHandler.java
+++ HMG/src/main/java/handmadeguns/HMGPacketHandler.java
@@ -55,6 +55,8 @@ public class HMGPacketHandler {
                 Side.SERVER);
         INSTANCE.registerMessage(MessageCatch_PlaySound_Gui.class, PacketPlaySound_Gui.class, ++id,
                 Side.CLIENT);
+        INSTANCE.registerMessage(MessageCatch_KillFeedEntry.class, PacketKillFeedEntry.class, ++id,
+                Side.CLIENT);
         INSTANCE.registerMessage(MessageCatch_SetElevation.class, PacketSetElevation.class, ++id,
                 Side.SERVER);
     }
